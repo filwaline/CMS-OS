f3fc1f885420445f89b91e403e561bcc559aba01
DarthScience <skywalker950925@gmail.com>
2016-06-28 11:45:16 +0800

    lesson 2
    kalman filter
    homework 2.6

--- Artificial Intelligence for Robotics/codes/homework2.6/task.py
+++ Artificial Intelligence for Robotics/codes/homework2.6/task.py
@@ -144,11 +144,18 @@ def calculate(measurements, initial_xy):
   u = matrix([[0.], [0.], [0.], [0.]]) # external motion
 
   ### fill this in: ###
-  P =  # initial uncertainty
-  F =  # next state function
-  H =  # measurement function
-  R =  # measurement uncertainty
-  I =  # identity matrix
+  P =  matrix([[0.,0.,0.,0.],
+              [0.,0.,0.,0.],
+              [0.,0.,1000.,0.],
+              [0.,0.,0.,1000.]]) # initial uncertainty
+  F =  matrix([[1.,0.,dt,0.],
+               [0.,1.,0.,dt],
+               [0.,0.,1.,0.],
+               [0.,0.,0.,1.]]) # next state function
+  H =  matrix([[1.,0.,0.,0.],
+               [0.,1.,0.,0.]]) # measurement function
+  R =  matrix([[dt,0],[0,dt]]) # measurement uncertainty
+  I =  matrix([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]) # identity matrix
 
   def filter(x, P):
     for n in range(len(measurements)):
